f8a2e86dbed42433ab3a298a36f1b1b0ac3997af
Maxime AUBLET <maximeaublet@gmail.com>
2018-02-05 20:08:00 +0100

    on click

--- app/src/main/java/com/andro/prolapso/shapi/views/MainActivity.java
+++ app/src/main/java/com/andro/prolapso/shapi/views/MainActivity.java
@@ -1,9 +1,13 @@
 package com.andro.prolapso.shapi.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.andro.prolapso.shapi.R;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void editProfile(View v) {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "EditProfileActivity");
+        startActivity(intent);
     }
 }
