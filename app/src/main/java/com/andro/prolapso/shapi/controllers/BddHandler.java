d01995ee13fb8ef1b20bcf5ccabebe8a6cf42f32
Maxime AUBLET <maximeaublet@gmail.com>
2018-02-11 15:54:35 +0100

    refonte exo

--- app/src/main/java/com/andro/prolapso/shapi/controllers/BddHandler.java
+++ app/src/main/java/com/andro/prolapso/shapi/controllers/BddHandler.java
@@ -25,23 +25,16 @@ class BddHandler extends SQLiteOpenHelper {
             ");";
 
     private static final String createTableType = "CREATE TABLE IF NOT EXISTS type(" +
-            " id_type       INTEGER         NOT NULL PRIMARY KEY AUTOINCREMENT," +
-            " name          VARCHAR(40)" +
+            " id_type       INTEGER         ," +
+            " name          VARCHAR(40)     " +
             ");";
 
-    private static final String createTableMuscle = "CREATE TABLE IF NOT EXISTS muscle(" +
-            " id_muscle     INTEGER         NOT NULL PRIMARY KEY AUTOINCREMENT," +
-            " name          VARCHAR(40)," +
-            " id_type       INTEGER," +
-            " FOREIGN KEY(id_type) REFERENCES type(id_type)" +
-            ");";
-
-    private static final String createTableExercice = "CREATE TABLE IF NOT EXISTS exercise(" +
+    private static final String createTableExercise = "CREATE TABLE IF NOT EXISTS exercise(" +
             " id_exo         INTEGER        NOT NULL PRIMARY KEY AUTOINCREMENT," +
             " name           VARCHAR(40)," +
             " description    VARCHAR(200)," +
-            " id_muscle      INTEGER," +
-            " FOREIGN KEY(id_muscle) REFERENCES muscle(id_muscle)" +
+            " id_type      INTEGER," +
+            " FOREIGN KEY(id_type) REFERENCES type(id_type)" +
             ");";
 
     private static final String createTableProgExo = "CREATE TABLE IF NOT EXISTS progexo(" +
@@ -58,9 +51,8 @@ class BddHandler extends SQLiteOpenHelper {
     private static final String TABLE_DROP_PROGRAM = "DROP TABLE IF EXISTS " + "program" + ";";
     private static final String TABLE_DROP_USER = "DROP TABLE IF EXISTS " + "user" + ";";
     private static final String TABLE_DROP_EXO = "DROP TABLE IF EXISTS " + "exercise" + ";";
-    private static final String TABLE_DROP_MUSCLE = "DROP TABLE IF EXISTS " + "muscle" + ";";
     private static final String TABLE_DROP_TYPE = "DROP TABLE IF EXISTS " + "type" + ";";
-    private static final String TABLE_DROP_PROGEXO= "DROP TABLE IF EXISTS " + "progexo" + ";";
+    private static final String TABLE_DROP_PROGEXO = "DROP TABLE IF EXISTS " + "progexo" + ";";
 
 
     public BddHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -72,8 +64,7 @@ class BddHandler extends SQLiteOpenHelper {
         db.execSQL(createUser);
         db.execSQL(createTableProgramme);
         db.execSQL(createTableType);
-        db.execSQL(createTableMuscle);
-        db.execSQL(createTableExercice);
+        db.execSQL(createTableExercise);
         db.execSQL(createTableProgExo);
 
         if (countUser(db) < 1) {
@@ -87,7 +78,6 @@ class BddHandler extends SQLiteOpenHelper {
         db.execSQL(TABLE_DROP_USER);
         db.execSQL(TABLE_DROP_PROGEXO);
         db.execSQL(TABLE_DROP_EXO);
-        db.execSQL(TABLE_DROP_MUSCLE);
         db.execSQL(TABLE_DROP_TYPE);
         db.execSQL(TABLE_DROP_PROGRAM);
         onCreate(db);
@@ -110,6 +100,28 @@ class BddHandler extends SQLiteOpenHelper {
         // Initialized
         Log.d("-- BDD_CLASS --", "INIT_USER : Initialized: " + res);
 
+    }
+
+    private void initType(SQLiteDatabase db) {
+
+        ContentValues vals = new ContentValues();
+        vals.put("id_type","1");
+        vals.put("name","Haut du corps");
+
+        long res = db.insert("type", null, vals);
+
+        vals.put("id_type","2");
+        vals.put("name","Tronc");
+
+        res = db.insert("type", null, vals);
+
+        vals.put("id_type","3");
+        vals.put("name","Jambes");
+
+        res = db.insert("type", null, vals);
+
+        // Initialized
+        Log.d("-- BDD_CLASS --", "INIT_TYPE : Initialized: " + res);
     }
 
     // Comptage du nombre d'user
